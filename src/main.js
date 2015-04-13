760dde19b4b6931f7f195c4c34586315fe2bd9f8
Emily Lutz <emily.lutz@berkeley.edu>
2015-04-12 19:53:16 -0700

    committed the right project... sorry guys

--- src/main.js
+++ src/main.js
@@ -1,32 +1,25 @@
 //@program
+
 var THEME = require("themes/flat/theme");
 var BUTTONS = require("controls/buttons");
+var style = new Style( { font: "bold 20px", color:"black" });
+var whiteSkin = new Skin( { fill:"white" } );
 
-var logging = false;
+/**** For Easier Logging ***/
+//Whether logging messages should be displayed or not
+var logging = true;
+//Displays message if logging turned on
 var myLog = function(message){if(logging){traceLine(message)}};
+//Displays message, appending a newline symbol
 var traceLine = function(message){trace(message+"\n")};
 
-var styleFromTemplate = function() { return new Style( { font: "bold 20px", color:"black" })};
-
+/**** Discovering Device ****/
 deviceURL = "";
-
-var logo = new Texture("fill.png");
-
-var logoSkin = new Skin({
-	width:100,
-	height:100,
-	texture: logo,
-	fill:"white"
-});
-
-var whiteSkin = new Skin( { fill:"white" } );
-var blueSkin = new Skin( { fill:"blue" } );
-var redSkin = new Skin( { fill:"red" } );
 
 Handler.bind("/discover", Behavior({
 	onInvoke: function(handler, message){
 		deviceURL = JSON.parse(message.requestText).url;
-		trace("discovered device");
+		traceLine("discovered device");
 	}
 }));
 
@@ -36,212 +29,163 @@ Handler.bind("/forget", Behavior({
 	}
 }));
 
-var alertLabel = new Label({left:0, right:0, height:40, string:"No Alerts", style: styleFromTemplate()});
+
+var goalTemp = 100;
+var curOvenTemp = 0;
+var timerLength = 100; //in milliseconds
 
 /* Alerts from device */
-Handler.bind("/foodLevelLow", Behavior({
+
+Handler.bind("/smokeDetectedAlert", Behavior({
 	onInvoke: function(handler, message){
-		myLog("phone received food level low alert \n");
-		treatsGiven = 0;
-		alertLabel.string = "ALERT: Food Level Below 10%";
-		/*message.responseText = JSON.stringify( { treatsGiven: "0" } );*/
-		message.status = 200;
-		myLog("phone done with water level low alert \n");
+		myLog("SMOKE DETECTED IN OVEN!");
+		smokeDetectedLabel.string = "True";
 	}
 }));
-Handler.bind("/foodLevelOK", Behavior({
+Handler.bind("/smokeDetectedAllClear", Behavior({
 	onInvoke: function(handler, message){
-		myLog("phone received food level ok alert \n");
-		treatsGiven = 0;
-		alertLabel.string = "Food Dispensor Level above 10%";
-		/*message.responseText = JSON.stringify( { treatsGiven: "0" } );*/
-		message.status = 200;
-		myLog("phone done with water level ok alert \n");
+		myLog("Smoke detector all clear.");
+		smokeDetectedLabel.string = "False";
 	}
 }));
 
-/* Dispenser Level Updates from Device */
-Handler.bind("/updateWaterLevel", Behavior({
-	onInvoke: function(handler, message){
-		myLog("phone received update water level \n");
-		var level = message.requestText*1;
-		waterLevelBlock.height = message.requestText*containerHeight;
-		waterLevelLabel.string = level.toFixed(2)+"%"
-		myLog("phone done with update water level \n");
-	}
-}));
-Handler.bind("/updateFoodLevel", Behavior({
-	onInvoke: function(handler, message){
-		myLog("phone received update food level \n");
-		var level = message.requestText*1;
-		foodLevelBlock.height = message.requestText*containerHeight;
-		foodLevelLabel.string = level.toFixed(2)+"%"
-		myLog("phone done with update food level \n");
-	}
-}));
-Handler.bind("/updateTreatLevel", Behavior({
-	onInvoke: function(handler, message){
-		myLog("phone received update treat level \n");
-		var level = message.requestText*1;
-		treatLevelBlock.height = message.requestText*containerHeight;
-		treatLevelLabel.string = level.toFixed(2)+"%"
-		myLog("phone done with update treat level \n");
+Handler.bind("/gotCurrentTemp", Behavior({
+	onInvoke: function(handler, message, json){
+		//myLog("Got current oven temp.");
+		if(message.requestText){
+			curOvenTemp = message.requestText;
+		} else {
+			curOvenTemp = json.curTemp;
+		}
+		currentOvenTempLabel.string = curOvenTemp+"*F";
 	}
 }));
 
-var waterBehavior = Behavior({
-						onTouchBegan: function(container, id, x, y, ticks){
-								container.style.size = container.style.size - 2;
-						},
-						onTouchEnded: function(container, content){
-							container.style.size = container.style.size + 2;
-							if (deviceURL != "") content.invoke(new Message(deviceURL + "giveWater"), Message.JSON);
-						},
-						onComplete: function(content, message, json){
-							waterCounterLabel.string = json.treatsGiven;
-						}
-					});
-var foodBehavior = Behavior({
-						onTouchBegan: function(container, id, x, y, ticks){
-								container.style.size = container.style.size - 2;
-						},
-						onTouchEnded: function(container, content){
-							container.style.size = container.style.size + 2;
-							if (deviceURL != "") content.invoke(new Message(deviceURL + "giveFood"), Message.JSON);
-						},
-						onComplete: function(content, message, json){
-							foodCounterLabel.string = json.treatsGiven;
-						}
-					});
-var treatBehavior = new Behavior({
-						onTouchBegan: function(container, id, x, y, ticks){
-								container.style.size = container.style.size - 2;
-						},
-						onTouchEnded: function(container, content){
-							container.style.size = container.style.size + 2;
-							if (deviceURL != "") content.invoke(new Message(deviceURL + "giveTreat"), Message.JSON);
-						},
-						onComplete: function(content, message, json){
-							treatCounterLabel.string = json.treatsGiven;
-						}
-					});
-var waterCounterLabel = new Label({left:0, right:0, height:40, string:"0", style: styleFromTemplate()});
-var foodCounterLabel = new Label({left:0, right:0, height:40, string:"0", style: styleFromTemplate()});
-var treatCounterLabel = new Label({left:0, right:0, height:40, string:"0", style: styleFromTemplate()});
+Handler.bind("/gotPicture", Behavior({
+	onInvoke: function(handler, message){
+		myLog("Got picture");
+		//Picture will come in here
+	}
+}));
 
-var containerHeight = 70;
-var waterLevelBlock = new Content({left:0, right:0, bottom:0, height:containerHeight, skin: blueSkin}),
-var foodLevelBlock = new Content({left:0, right:0, bottom:0, height:containerHeight, skin: blueSkin}),
-var treatLevelBlock = new Content({left:0, right:0, bottom:0, height:containerHeight, skin: blueSkin}),
-
-var waterStyle = styleFromTemplate();
-var foodStyle = styleFromTemplate();
-var treatStyle = styleFromTemplate();
-
-var waterLevelLabel = new Label({left:0, right:0, height:20, string:"100%", style: waterStyle}),
-var foodLevelLabel = new Label({left:0, right:0, height:20, string:"100%", style: foodStyle}),
-var treatLevelLabel = new Label({left:0, right:0, height:20, string:"100%", style: treatStyle}),
-
-
+var ovenStatusLabel = new Label({left:0, right:0, height:20, string:"OFF", style: style}),
+var currentOvenTempLabel = new Label({left:0, right:0, height:20, string:"0", style: style}),
+var goalTempLabel = new Label({left:0, right:0, height:20, string:"0", style: style}),
+var smokeDetectedLabel = new Label({left:0, right:0, height:20, string:"False", style: style}),
+var timerLengthLabel = new Label({left:0, right:0, height:20, string:"0", style: style}),
 
 var mainColumn = new Column({
 	left: 0, right: 0, top: 0, bottom: 0, active: true, skin: whiteSkin,
 	contents: [
-		new Line({left:0, right:0, height:30, skin: logoSkin}),
 		new Line({left:0, right:0, skin: whiteSkin,
 			contents:[
-				new Label({left:0, right:0, height:20, string:"Water", style: waterStyle}),
-				new Label({left:0, right:0, height:20, string:"Food", style: foodStyle}),
-				new Label({left:0, right:0, height:20, string:"Treats", style: treatStyle}),
+				new Label({left:0, right:0, height:20, string:"Oven Status", style: style}),
+				ovenStatusLabel
 			]}),
-		new Line({left:0, right:0, height:containerHeight, skin: whiteSkin,
-			contents:[
-				new Container({left:10, right:5, top:0, bottom:0, skin: redSkin,
-					contents: waterLevelBlock}),
-				new Container({left:10, right:5, top:0, bottom:0, skin: redSkin,
-					contents: foodLevelBlock}),
-				new Container({left:10, right:5, top:0, bottom:0, skin: redSkin,
-					contents: treatLevelBlock}),
-			]
-		}),
 		new Line({left:0, right:0, skin: whiteSkin,
 			contents:[
-				waterLevelLabel,
-				foodLevelLabel,
-				treatLevelLabel
+				new Label({left:0, right:0, height:20, string:"Current Oven Temp", style: style}),
+				currentOvenTempLabel
 			]}),
-		new Line({left:0, right:0, height:30, skin: logoSkin}),
+		new Line({left:0, right:0, skin: whiteSkin,
+			contents:[
+				new Label({left:0, right:0, height:20, string:"Oven Goal Temp", style: style}),
+				goalTempLabel
+			]}),
+		new Line({left:0, right:0, skin: whiteSkin,
+			contents:[
+				new Label({left:0, right:0, height:20, string:"Smoke Detected?", style: style}),
+				smokeDetectedLabel
+			]}),
+		new Line({left:0, right:0, skin: whiteSkin,
+			contents:[
+				new Label({left:0, right:0, height:20, string:"Timer Length", style: style}),
+				timerLengthLabel
+			]}),
 		
-		new Line({left:0, right:0, skin: whiteSkin,
-			contents:[
-				new Label({left:0, right:0, height:20, string:" /~~~\\ ", style: waterStyle, active: "true",
+		new Label({left:0, right:0, height:20, string:"SET GOAL TEMP", style: style, active: "true",
 					behavior: Behavior({
-						onTouchBegan: function(container, id, x, y, ticks){
-								container.style.size = container.style.size - 2;
-						},
 						onTouchEnded: function(container, content){
-							container.style.size = container.style.size + 2;
-							if (deviceURL != "") container.invoke(new Message(deviceURL + "giveWater"), Message.JSON);
+							var message = new Message(deviceURL + "setGoalTemp");
+							message.requestText = goalTemp;
+							if (deviceURL != "") container.invoke(message, Message.JSON);
 						},
 						onComplete: function(content, message, json){
-							waterCounterLabel.string = json.waterGiven;
+							goalTempLabel.string = json.goalTemp;
 						}
 					})
 					}),
-				new Label({left:0, right:0, height:20, string:" /***\\ ", style: foodStyle, active: "true",
+		new Label({left:0, right:0, height:20, string:"TURN ON", style: style, active: "true",
+			behavior: Behavior({
+				onTouchEnded: function(container, content){
+					if (deviceURL != "") container.invoke(new Message(deviceURL + "turnOnOven"), Message.JSON);
+				},
+				onComplete: function(content, message, json){
+					ovenStatusLabel.string = json.ovenOn;
+				}
+			})}),
+		new Label({left:0, right:0, height:20, string:"TURN OFF", style: style, active: "true",
+			behavior: Behavior({
+				onTouchEnded: function(container, content){
+					if (deviceURL != "") container.invoke(new Message(deviceURL + "turnOffOven"), Message.JSON);
+				},
+				onComplete: function(content, message, json){
+					ovenStatusLabel.string = json.ovenOn;
+				}
+			})}),
+		new Label({left:0, right:0, height:20, string:"SET TIMER", style: style, active: "true",
 					behavior: Behavior({
-						onTouchBegan: function(container, id, x, y, ticks){
-								container.style.size = container.style.size - 2;
-						},
 						onTouchEnded: function(container, content){
-							container.style.size = container.style.size + 2;
-							if (deviceURL != "") container.invoke(new Message(deviceURL + "giveFood"), Message.JSON);
+							var message = new Message(deviceURL + "setTimer");
+							message.requestText = timerLength;
+							if (deviceURL != "") container.invoke(message, Message.JSON);
 						},
 						onComplete: function(content, message, json){
-							foodCounterLabel.string = json.foodGiven;
+							timerLengthLabel.string = json.timerLength;
 						}
-					})}),
-				new Label({left:0, right:0, height:20, string:" /_*_\\ ", style: treatStyle, active: "true",
-					behavior: Behavior({
-						onTouchBegan: function(container, id, x, y, ticks){
-								container.style.size = container.style.size - 2;
-						},
-						onTouchEnded: function(container, content){
-							container.style.size = container.style.size + 2;
-							if (deviceURL != "") container.invoke(new Message(deviceURL + "giveTreat"), Message.JSON);
-						},
-						onComplete: function(content, message, json){
-							treatCounterLabel.string = json.treatsGiven;
-						}
-					})})
-			]}),
-		new Line({left:0, right:0, height:30, skin: whiteSkin}),
-
-		new Line({left:0, right:0, skin: whiteSkin,
-			contents:[
-				new Label({left:0, right:0, height:40, string:"Water Sent:", style: styleFromTemplate()}),
-				new Label({left:0, right:0, height:40, string:"Food Sent:", style: styleFromTemplate()}),
-				new Label({left:0, right:0, height:40, string:"Treats Sent:", style: styleFromTemplate()}),
-			]}),
-		new Line({left:0, right:0, skin: whiteSkin,
-			contents:[
-				waterCounterLabel,
-				foodCounterLabel,
-				treatCounterLabel
-			]}),
-		alertLabel
-	]
+					})
+					}),
+		new Label({left:0, right:0, height:20, string:"GET PICTURE", style: style, active: "true",
+			behavior: Behavior({
+				onTouchEnded: function(container, content){
+					if (deviceURL != "") container.invoke(new Message(deviceURL + "getPicture"), Message.JSON);
+				},
+				onComplete: function(content, message, json){
+					//picture will be received here
+					//until this is finsihed, just use a picture in the same folder as main.js
+				}
+			})})
+		]
 });
 
+/* Calling device constantly */
+/*
+application.invoke( new MessageWithObject( deviceURL + "getPicture" + 
+			serializeQuery( {
+				repeat: "on",
+				interval: 20,
+				callback: "/gotPicture"
+		} ) ) );
+*/
+
+//This isn't working quite yet, but I will keep working on it
+/*
+application.invoke( new MessageWithObject( deviceURL + "getCurrentOvenTemp" + 
+			serializeQuery( {
+				repeat: "on",
+				interval: 20,
+				callback: "/gotCurrentTemp"
+		} ) ) );
+*/
 var ApplicationBehavior = Behavior.template({
 	onLaunch: function(application) {
 		application.shared = true;
 	},
 	onDisplayed: function(application) {
-		application.discover("smartfeederdevice.app");
+		application.discover("smartovendevice.app");
 	},
 	onQuit: function(application) {
-		application.forget("smartfeederdevice.app");
+		application.forget("smartovendevice.app");
 		application.shared = false;
 	},
 })
